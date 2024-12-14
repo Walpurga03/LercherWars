01d3bf1abee8a21f559735710adafe9f745d86a4
alex <alex.moik@gmx.at>
2024-12-14 19:19:51 +0100

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Dynamische Basis-URL für die Entwicklung und Produktion
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/cardBattle/' : '/', // Basis-URL dynamisch setzen
+  base: '/lercherWars/',
 });
